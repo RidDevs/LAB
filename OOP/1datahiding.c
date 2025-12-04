17452a2c56de15394701c970b67055144d1ad440
RidDevs <127176942+RidDevs@users.noreply.github.com>
2025-12-04 16:59:17 +0530

    oop1

---
+++ OOP/1datahiding.c
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class game {
+    private:
+        int score;   // here, data hiding is used
+    public:
+        void setScore(int s) {
+            score = s;
+        }
+        void showScore() {
+            cout <<"Score = "<< score;
+        }
+};
+
+int main() {
+    game g;
+    g.setScore(85);
+    g.showScore();
+    return 0;
+}
